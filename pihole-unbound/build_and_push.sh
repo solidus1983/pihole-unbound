9e8f831fc92a0a0321986442f23f9d7e64086acd
solidus1983 <8018745+solidus1983@users.noreply.github.com>
2022-03-08 15:01:40 +0000

    Update build_and_push.sh

--- pihole-unbound/build_and_push.sh
+++ pihole-unbound/build_and_push.sh
@@ -1,8 +1,7 @@
+
 #!/bin/bash
 # Run this once: docker buildx create --use --name build --node build --driver-opt network=host
 # Run this once: 
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t solidus1983/pihole-unbound:`cat VERSION` --push .
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t solidus1983/pihole-unbound:latest --push .
-
-
